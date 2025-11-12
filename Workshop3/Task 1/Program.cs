6a614b931ae761e5e5e6532b6d2af0d88c3123da
AryanChd <chaudharyaryancr7@gmail.com>
2025-11-12 08:49:52 +0545

    added workshop3 folder done task 1

---
+++ Workshop3/Task 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace workshop3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operators op = new Operators();
+
+            Console.Write("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Call all arithmetic methods
+            op.Add(num1, num2);
+            op.Subtract(num1, num2);
+            op.Multiply(num1, num2);
+            op.Divide(num1, num2);
+
+            // Odd or even check
+            Console.Write("\nEnter a number to check if it's odd or even: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            op.OddEvenFinder(number);
+
+            Console.ReadLine(); // Keeps console window open
+        }
+    }
+}
